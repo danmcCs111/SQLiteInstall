2c3227ddcab63f138730b2609d010842892a034d
danmcCs111 <danielboydelatour@gmail.com>
2026-01-10 01:29:47 -0500

    initial script add

---
+++ buildLite.cmd
@@ -0,0 +1,1 @@
+C:\WINDOWS\system32\cmd.exe /k "C:\Program Files\Microsoft Visual Studio\18\Community\Common7\Tools\VsDevCmd.bat" 
---
+++ downloadLite.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+curl https://sqlite.org/2026/sqlite-src-3510200.zip --output sqlite-src-3510200.zip 
+unzip sqlite-src-3510200.zip
