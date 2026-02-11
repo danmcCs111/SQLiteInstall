7156b5075569d6e4649816f5bc870bdd6eac3b85
danmcCs111 <danmccs111@gmail.com>
2026-02-11 18:15:24 -0500

    update downloadLite.sh directory

--- install/downloadLite.sh
+++ install/downloadLite.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
+orgDir=`pwd`
+cd "$(dirname "$0")"
+cd ..
 curl https://sqlite.org/2026/sqlite-src-3510200.zip --output sqlite-src-3510200.zip 
 unzip sqlite-src-3510200.zip
+cd $orgDir
