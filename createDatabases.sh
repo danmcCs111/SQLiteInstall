ba84a93e3ce3389f4ab2be796ffcc888faf340e2
danmcCs111 <danmccs111@gmail.com>
2026-07-09 01:02:09 -0400

    corrections to create shell script

--- createDatabases.sh
+++ createDatabases.sh
@@ -2,27 +2,35 @@
 sqlite="$1"
 
 prefix="../DatabaseAdapterService/sql_statements/create/create-tables/*-sqlite/"
-createTables=(`ls $prefix`)
+createTables=(`ls -l $prefix | grep :$`)
 
 var=""
 count=${#createTables[@]}
-countInc=$(( $count / 2 ))
 
-echo $count "${createTables[@]}"
-count=0
+echo ${createTables[@]}
 
-for i in {0..$(( $countInc ))}
+for i in $(seq 0 $(( $count-1 )) )
 do
-	dir=`echo ${createTables[$(($count))]} | sed 's/://g'`
-	var=`cat "$dir${createTables[$(( $count + 1 ))]}"`
-	database=`echo $dir | egrep -o "[a-zA-Z\-]*/$" | sed 's/table//g' | sed 's/sqlite//g' | egrep -o "[a-zA-Z]+"`
-	database=`echo ${database,,}`.db
+	dir=`echo ${createTables[$(($i))]} | sed 's/://g'`
+	database=`echo $dir | egrep -o "[a-zA-Z\-]*/$" | sed 's/table-//g' | sed 's/sqlite//g' | egrep -o "[a-zA-Z]*"`
+	database=${database,,}.db
+	
+	echo $dir
+	fs=(`ls $dir`)
+	echo ${fs[@]}
 
 	echo dir: "$dir" 
 	echo database: "$database" 
-	echo table: "$var" 
 
-	echo "$var" | $sqlite $database
-	count=$(( $count + 2 ))
+	for f in ${fs[@]}
+	do
+		fName=$dir$f
+		sql=`cat $fName`
+
+		echo sqlFileName $fName
+		echo sqlfileContents $sql
+		
+		echo "$sql" | $sqlite $database
+	done
 done
 
