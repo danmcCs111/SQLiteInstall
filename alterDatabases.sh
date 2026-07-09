832f3000d802e803ed92e9a9358e37c16d32908d
danmcCs111 <danmccs111@gmail.com>
2026-07-09 01:15:01 -0400

    update to use parent script

--- alterDatabases.sh
+++ alterDatabases.sh
@@ -2,35 +2,5 @@
 sqlite="$1"
 
 prefix="../DatabaseAdapterService/sql_statements/alter/alter-tables/*-sqlite/"
-createTables=(`ls -l $prefix | grep :$`)
 
-var=""
-count=${#createTables[@]}
-
-echo ${createTables[@]}
-
-for i in $(seq 0 $(( $count-1 )) )
-do
-	dir=`echo ${createTables[$(($i))]} | sed 's/://g'`
-	database=`echo $dir | egrep -o "[a-zA-Z\-]*/$" | sed 's/table-//g' | sed 's/sqlite//g' | egrep -o "[a-zA-Z]*"`
-	database=${database,,}.db
-	
-	echo $dir
-	fs=(`ls $dir`)
-	echo ${fs[@]}
-
-	echo dir: "$dir" 
-	echo database: "$database" 
-
-	for f in ${fs[@]}
-	do
-		fName=$dir$f
-		sql=`cat $fName`
-
-		echo sqlFileName $fName
-		echo sqlfileContents $sql
-		
-		echo "$sql" | $sqlite $database
-	done
-done
-
+./database-manage.sh $sqlite "$prefix"
--- createDatabases.sh
+++ createDatabases.sh
@@ -2,36 +2,7 @@
 sqlite="$1"
 
 prefix="../DatabaseAdapterService/sql_statements/create/create-tables/*-sqlite/"
-createTables=(`ls -l $prefix | grep :$`)
 
-var=""
-count=${#createTables[@]}
-
-echo ${createTables[@]}
-
-for i in $(seq 0 $(( $count-1 )) )
-do
-	dir=`echo ${createTables[$(($i))]} | sed 's/://g'`
-	database=`echo $dir | egrep -o "[a-zA-Z\-]*/$" | sed 's/table-//g' | sed 's/sqlite//g' | egrep -o "[a-zA-Z]*"`
-	database=${database,,}.db
-	
-	echo $dir
-	fs=(`ls $dir`)
-	echo ${fs[@]}
-
-	echo dir: "$dir" 
-	echo database: "$database" 
-
-	for f in ${fs[@]}
-	do
-		fName=$dir$f
-		sql=`cat $fName`
-
-		echo sqlFileName $fName
-		echo sqlfileContents $sql
-		
-		echo "$sql" | $sqlite $database
-	done
-done
+./database-manage.sh $sqlite "$prefix"
 
 ./alterDatabases.sh $sqlite
