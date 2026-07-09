97a83540eaceeab2f90b948b507f08bb9fd7d77e
danmcCs111 <danmccs111@gmail.com>
2026-07-09 10:49:40 -0400

    accidently removed sqlite3 arg

--- createDatabases.sh
+++ createDatabases.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
-sqlite="$1"
+typeOs=`uname`
+if [[ "$typeOs" == "Linux" ]]
+then
+	sqlite="./sqlite-src-3510200/sqlite3"
+else
+	sqlite="./sqlite-src-3510200/sqlite3.exe" 
+fi
 
 prefix="../DatabaseAdapterService/sql_statements/create/create-tables/*-sqlite/"
 
