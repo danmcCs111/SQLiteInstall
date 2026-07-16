46b02d7be66847e0c04e14a3059eaa54c13f3eae
danmcCs111 <danmccs111@gmail.com>
2026-07-15 20:23:49 -0400

    include sqlite tools.

--- install/downloadLite.sh
+++ install/downloadLite.sh
@@ -4,4 +4,16 @@ cd "$(dirname "$0")"
 cd ..
 curl https://sqlite.org/2026/sqlite-src-3510200.zip --output sqlite-src-3510200.zip 
 unzip sqlite-src-3510200.zip
+
+if [[ "$typeOs" == "Linux" ]]
+then
+	curl https://sqlite.org/2026/sqlite-tools-linux-x64-3530300.zip --output sqlite-tools-linux-x64-3510200.zip
+	unzip sqlite-tools-linux-x64-3510200.zip -d sqlite-tools-linux-x64-3510200
+else
+	curl https://sqlite.org/2026/sqlite-tools-win-x64-3530300.zip --output sqlite-tools-win-x64-3510200.zip
+	unzip sqlite-tools-win-x64-3510200.zip -d sqlite-tools-win-x64-3510200
+fi
 cd $orgDir
+
+
+
